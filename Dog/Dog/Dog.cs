6459b02f7369191d0f11d2abb44a1e913941d02f
MSharp813 <51858295+MSharp813@users.noreply.github.com>
2019-11-25 18:01:11 -0500

    Add files via upload

---
+++ Dog/Dog/Dog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dog
+{
+    class Dog
+    {
+        public string bark { get; set; }
+        // Add bark() method
+        public string bark; 
+        public static void bark(); 
+        { 
+            Console .WriteLine( "{0} is Barking..." , Name); 
+        }
+
+
+        // Add doTrick() method
+        public string doTrick; 
+        public static void doTrick(trickName); 
+        { 
+            Console.WriteLine( "{0} is so smart! {0} is doing a(n) {1}" , name, trickName);
+        }
+    }
+}
